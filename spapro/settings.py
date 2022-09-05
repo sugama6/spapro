54f7e62aa672211c41824ced70c2a0d4b8a1b1e8
YusukeSugama <yusuke_tc_6@ezweb.ne.jp>
2022-09-05 09:33:49 +0900

    修正

--- spapro/settings.py
+++ spapro/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sk8yusuke777@gmail.com'
-DEFAULT_FROM_EMAIL = 'sk8yusuke777@gmail.com'
+EMAIL_HOST_USER = ''
+DEFAULT_FROM_EMAIL = ''
 
-EMAIL_HOST_PASSWORD = 'knzlzfvabycgnsyt'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
